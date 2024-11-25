372df2f5066faf602142f8357b00ece24f5edeb9
anger issues <skuenemy@gmail.com>
2024-11-25 18:44:33 +0300

    update

--- src/backend/db/queries.go
+++ src/backend/db/queries.go
@@ -1,13 +1,1 @@
 package db
-
-import (
-	"database/sql"
-
-	_ "github.com/lib/pq"
-)
-
-func ConnectDB() (*sql.DB, error){ 
-	connStr := "host=localhost port=5433 user=admin password='Zxczxczxc1' dbname=postgres sslmode=disable"
-    return sql.Open("postgres", connStr)
-    
-}
